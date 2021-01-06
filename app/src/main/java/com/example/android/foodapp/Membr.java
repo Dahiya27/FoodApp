6d8591ed54cfa78fd61e459ca87dcb861dba88f5
ASHISH-KUMARSONI <ashishsoni91717@gmail.com>
2021-01-06 13:16:49 +0530

    firestore related data

---
+++ app/src/main/java/com/example/android/foodapp/Membr.java
@@ -0,0 +1,22 @@
+package com.example.android.foodapp;
+
+public class Membr {
+    String title,image;
+    public  Membr(){}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
---
+++ app/src/main/java/com/example/android/foodapp/ui/home/recycleviewadapter.java
@@ -0,0 +1,68 @@
+package com.example.android.foodapp.ui.home;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+//import com.bumptech.glide.Glide;
+import com.bumptech.glide.Glide;
+import com.example.android.foodapp.Membr;
+import com.example.android.foodapp.R;
+
+import java.util.ArrayList;
+
+public class recycleviewadapter extends RecyclerView.Adapter<recycleviewadapter.MyViewHolder> {
+    ArrayList<Membr>ar;
+    Context context;
+
+    public recycleviewadapter(ArrayList<Membr> ar, Context context) {
+        this.ar = ar;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.imagesingle,parent,false);
+        MyViewHolder holder=new MyViewHolder(view);
+        Log.w("hu hu","here");
+        return  holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+       // holder.ttitle.setText(ar.get(position).getTitle());
+        String th="thishsh is ashish";
+     //   holder.ttitl.setText(th);
+        Log.w("ha ha","here  "+ar.get(0).getTitle());
+        //Glide.with(this.context).load(ar.get(position).getImage()).into(holder.mimg);
+    }
+
+    @Override
+    public int getItemCount() {
+        return ar.size() ;
+    }
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+       public TextView ttitl;
+       public ImageView mimg;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            TextView  ttitl= (TextView) itemView.findViewById(R.id.tvTitle);
+             //Log.w("one more","  "+ttitl.setText("ashish"));
+            ttitl.setText("check code in recyleviewapdapter.java..");
+
+            ImageView mimg= (ImageView) itemView.findViewById(R.id.ivshop);
+            Glide.with(context).load(ar.get(0).getImage()).into(mimg);
+        }
+    }
+
+
+}
